be7920dcd8401b19dd8340ae7c4d776570a067b9
Jann Mallek <126959900+JannMallek@users.noreply.github.com>
2023-08-07 23:45:02 +0200

    fixed bug + store logic for canvas

--- src/Terminal.jsx
+++ src/Terminal.jsx
@@ -1,23 +1,31 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { gsap } from 'gsap'
 
-export default function Terminal() {
+import useStat from './useStat'
+
+const stateOptions = {
+  
+}
+
+export default function Terminal({}) {
     const container = useRef()
- useEffect(()=>{
-    gsap.to(container.current, {autoAlpha:1}) //fouc
+    const [currentData, setCurrentData] = useState(null)
+    const { data, mode,  addData, removeData, setMode} = useStat()
+    useEffect(()=>{
+      gsap.to(container.current, {autoAlpha:1}) //fouc
         createTween(".hover.first", ".btn-flex.first")
         createTween(".hover.second", ".btn-flex.second")
         
-
         let activeButton = 1
         document.querySelectorAll(".btn-flex").forEach((button, index)=>{
             button.addEventListener("click", ()=>{
-                console.log("go")
-                if(activeButton != index){
-                    document.querySelector(".active").classList.remove("active")
-                    activeButton = index
-                    let btn = activeButton!=1 ? ".first" : ".second"
-                    document.querySelector(btn).classList.add("active") 
+              console.log(document.querySelector(".active")) 
+                if(activeButton != index + 1){
+                  
+                    document.querySelector(".active").classList.remove("active", "btn-bg")
+                    activeButton = index+ 1
+                    let btn = activeButton!=2 ? ".first" : ".second"
+                    document.querySelector(btn).classList.add("active", "btn-bg")
 
                 }
               
@@ -28,8 +36,21 @@ export default function Terminal() {
   return (
     <div className='terminal bg-slate-50 my-8 rounded-xl'>
         <div ref={container} className='flex  rounded-xl' >
-            <button className='btn-flex active first text-2xl  flex justify-center font-bold  bg-gray-300'><div className='text-bg'>Type</div><span className='hover first'></span></button>
-            <button className='btn-flex  second text-2xl  flex justify-center font-bold  bg-gray-300'><div className='text-bg'>Data</div><span className='hover second'></span></button>
+            <button className='btn-flex btn-bg active first text-2xl  flex justify-center font-semibold  bg-gray-300'><div className=''>Type</div><span className='hover first'></span></button>
+            <button className='btn-flex        second text-2xl  flex justify-center font-semibold  bg-gray-300'><div className=''>Data</div><span className='hover second'></span></button>
+        </div>
+        <div className="content">
+          {
+            //TODO options: absolute or relative output, total value, color theme: pick for Each, palette 
+            //TODO changes on mode: chart options
+            data.forEach((element) => (
+              <div className='dataInput'>
+                {
+                  //TODO remove button, name and value input, color
+                }
+              </div>
+            ))
+          }
         </div>
     </div>
   )
