b5f6ce289aad66f557e03e6396a3e5eff1672559
Jann Mallek <jannmallek@web.de>
2023-09-01 12:14:25 +0200

    first visualisation/ shadow implementation

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react'
-import { Canvas, useThree } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 
 import { globalState } from './App.jsx'
 
 import World from './World.jsx'
 import DataContainer from './DataContainer.jsx'
+import { Perf } from 'r3f-perf'
 
 export default function CanvasCom() {
   //access contents: all data changes on input using state
@@ -12,7 +13,8 @@ export default function CanvasCom() {
   console.log(theme)
 
   return (
-    <Canvas className={`canvas mb-8 ${theme.colorTheme}`}>
+    <Canvas shadows  className={`canvas mb-8 ${theme.colorTheme}`}>
+      <Perf/>
       <DataContainer/>
       <World/>
     </Canvas>
--- src/Visualisation.jsx
+++ src/Visualisation.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
-import { Text } from '@react-three/drei'
+import { Text, RandomizedLight, AccumulativeShadows } from '@react-three/drei'
 
 export default function Visualisation({dataObject, position}) {
     console.log(dataObject)
   return (
-        <group position={[position, 0, position]}>
-            <mesh scale={[0, dataObject.value, 0]}>
-                <boxGeometry args={[1,1,1]}/>
-                <meshStandardMaterial/>
+    <>
+        <AccumulativeShadows position={[0, 0.01, 0]} temporal frames={100} color='white' colorBlend={2} toneMapped={true}  scale={10}>
+            <RandomizedLight amount={8} radius={4} ambient={0.5} intensity={1} position={[5, 5, -10]} bias={0.001} />
+        </AccumulativeShadows>
+        <group position={[0, 0, position]}>
+            <mesh castShadow position={[-3, dataObject.value / 100, position -1]}>
+                <boxGeometry args={[1, dataObject.value / 50, 1]}/>
+                <meshStandardMaterial color={'#00ff00'}/>
             </mesh>
-            <Text position={[position -1, 0, position - 1]}>
+            <Text scale={[0.5, 0.5, 0.5]} rotation={[-0.5* Math.PI, 0, 0]} position={[0, 0.02,position-1]}>
                 {
                     `${dataObject.content} : ${dataObject.value}`
                 }
             </Text>
         </group>
+    </>
     )
 }
